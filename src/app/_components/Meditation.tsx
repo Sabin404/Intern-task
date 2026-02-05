566b8162614a73c5211c62a98c18b0e4288de1aa
Sabin <paudelsabin0@gmail.com>
2026-02-05 11:38:49 +0545

    style: ui improvement

--- src/app/_components/Meditation.tsx
+++ src/app/_components/Meditation.tsx
@@ -57,80 +57,80 @@ const Meditation = () => {
     },
   ];
   return (
-      <section className="py-16 container mx-auto lg:block hidden ">
-        <div className=" px-6">
-          <h1 className="display-medium font-semibold text-text-primary-hero text-center mb-16 font-robotoslab">
-            Why Practice Yoga & Meditation?
-          </h1>
-          <div className="flex flex-col md:flex-row items-center justify-center">
-            <div className="flex flex-1 flex-col gap-18 md:items-end">
-              {leftItems.map((item) => (
-                <div
-                  key={item.id}
-                  className="flex items-center gap-4 text-right min-h-22.5"
-                >
-                  <div className="flex flex-col gap-1">
-                    <h3 className="headline-small font-robotoslab font-semibold text-text-primary-hero whitespace-pre-line">
-                      {item.title}
-                    </h3>
-                    <p className="body-large font-roboto text-gray-600 whitespace-pre-line">
-                      {item.description}
-                    </p>
-                  </div>
-                  <div className="bg-bg-button-primary w-16 h-16 flex items-center justify-center rounded-xl shrink-0">
-                    <div className="w-10 h-10">
-                      <item.Icon />
-                    </div>
+    <section className="py-16 container mx-auto lg:block hidden ">
+      <div className=" px-6">
+        <h1 className="display-medium font-semibold text-text-primary-hero text-center mb-16 font-robotoslab">
+          Why Practice Yoga & Meditation?
+        </h1>
+        <div className="flex flex-col md:flex-row items-center justify-center">
+          <div className="flex flex-1 flex-col gap-18 md:items-end">
+            {leftItems.map((item) => (
+              <div
+                key={item.id}
+                className="flex items-center gap-4 text-right min-h-22.5"
+              >
+                <div className="flex flex-col gap-1">
+                  <h3 className="headline-small font-robotoslab font-semibold text-text-primary-hero whitespace-pre-line">
+                    {item.title}
+                  </h3>
+                  <p className="body-large font-roboto text-gray-600 whitespace-pre-line">
+                    {item.description}
+                  </p>
+                </div>
+                <div className="bg-bg-button-primary w-16 h-16 flex items-center justify-center rounded-xl shrink-0">
+                  <div className="w-10 h-10">
+                    <item.Icon />
                   </div>
                 </div>
-              ))}
-            </div>
-            <div className="flex flex-1 justify-center relative w-90 h-[120.75px]">
-              {/* Background Image */}
-              <Image
-                src={bgimage}
-                alt="Background Rotation"
-                width={360}
-                style={{ animationDuration: "15s" }}
-                height={360}
-                className="object-contain absolute -top-45 left-15 z-0  "
-              />
+              </div>
+            ))}
+          </div>
+          <div className="flex flex-1 justify-center relative w-90 h-[120.75px]">
+            {/* Background Image */}
+            <Image
+              src={bgimage}
+              alt="Background Rotation"
+              width={360}
+              style={{ animationDuration: "15s" }}
+              height={360}
+              className="object-contain absolute -top-45 left-15 z-0  "
+            />
 
-              {/* Front Image */}
-              <Image
-                src={frontImage}
-                alt="Front Illustration"
-                width={319}
-                height={441}
-                className="object-contain absolute -top-30 left-20 z-10 "
-              />
-            </div>
+            {/* Front Image */}
+            <Image
+              src={frontImage}
+              alt="Front Illustration"
+              width={319}
+              height={441}
+              className="object-contain absolute -top-30 left-20 z-10 "
+            />
+          </div>
 
-            <div className="flex flex-1 flex-col gap-18 md:items-start  ">
-              {rightItems.map((item) => (
-                <div
-                  key={item.id}
-                  className="flex items-center gap-4 text-left min-h-22.5"
-                >
-                  <div className="bg-bg-button-primary w-16 h-16 flex items-center justify-center rounded-xl shrink-0">
-                    <div className="w-10 h-10">
-                      <item.Icon />
-                    </div>
-                  </div>
-                  <div className="flex flex-col gap-1">
-                    <h3 className="headline-small font-robotoslab font-semibold text-text-primary-hero">
-                      {item.title}
-                    </h3>
-                    <p className="body-large font-roboto text-gray-600 whitespace-pre-line">
-                      {item.description}
-                    </p>
+          <div className="flex flex-1 flex-col gap-18 md:items-start  ">
+            {rightItems.map((item) => (
+              <div
+                key={item.id}
+                className="flex items-center gap-4 text-left min-h-22.5"
+              >
+                <div className="bg-bg-button-primary w-16 h-16 flex items-center justify-center rounded-xl shrink-0">
+                  <div className="w-10 h-10">
+                    <item.Icon />
                   </div>
                 </div>
-              ))}
-            </div>
+                <div className="flex flex-col gap-1">
+                  <h3 className="headline-small font-robotoslab font-semibold text-text-primary-hero">
+                    {item.title}
+                  </h3>
+                  <p className="body-large font-roboto text-gray-600 whitespace-pre-line">
+                    {item.description}
+                  </p>
+                </div>
+              </div>
+            ))}
           </div>
         </div>
-      </section>
+      </div>
+    </section>
   );
 };
 export default Meditation;
